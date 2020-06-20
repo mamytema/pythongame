38595d7e296b7dbf4d5f3320e639bd18a01e5b9a
mamytema <64401167+mamytema@users.noreply.github.com>
2020-06-20 20:51:12 +0300

    v.0.0.4

---
+++ config.py
@@ -0,0 +1,11 @@
+# This is the config
+# Change the values in here.
+
+savekey = "vJKYuupetc0LpjU97Hrq" # this is for saving your key file
+
+
+
+# DO NOT EDIT PAST THIS
+def getdata():
+    tbl = [savekey]
+    return tbl
